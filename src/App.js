16f9de5076df97ed351526ac4c6309f67ca78291
addy <adityakarmokar2000@gmail.com>
2024-11-10 17:30:33 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 //import './App.css';
 import './index.css';
-import './StudentReg/Studentform.css';
-import StudentForm from './StudentReg/Studentform';
+import SchoolReg from './registrationForm/SchoolReg.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Student Registration</h1>
-        <StudentForm />
+        <SchoolReg />
       </header>
     </div>
   );
